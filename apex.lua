6fc7c4eeb0bd462011c93997a4e7df8c189ad8f7
0Y0X <154022703+0y0x@users.noreply.github.com>
2025-09-27 10:08:36 -0400

    Update apex.lua

--- apex.lua
+++ apex.lua
@@ -1,8 +1,19 @@
-if game.PlaceId == "6403373529" then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/0y0x/apex/refs/heads/main/6403373529.lua",true))()
+-- Auto re-run script after teleport
+local queue_on_teleport = queue_on_teleport or syn and syn.queue_on_teleport or nil
+
+if queue_on_teleport then
+	queue_on_teleport([[
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/0y0x/apex/refs/heads/main/loader.lua"))()
+    ]])
 end
-if game.PlaceId == "6872265039" then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/0y0x/apex/refs/heads/main/6872265039.lua",true))()
+
+-- Loader logic
+local placeId = game.PlaceId
+
+if placeId == 6403373529 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/0y0x/apex/refs/heads/main/6403373529.lua", true))()
+elseif placeId == 6872265039 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/0y0x/apex/refs/heads/main/6872265039.lua", true))()
 else
-	print("Ivalid game, this game dose not have a script for it!")
+	print("Invalid game, this game does not have a script for it!")
 end
